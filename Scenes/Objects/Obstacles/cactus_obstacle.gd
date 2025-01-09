3fbafd560689642f7bc505b7705b5d5adb27b27c
how2fps <hearthstonesigh@gmail.com>
2025-01-09 21:43:40 +0800

    Added temp hi-scores, random interval obstacle spawning

--- Scenes/Objects/Obstacles/cactus_obstacle.gd
+++ Scenes/Objects/Obstacles/cactus_obstacle.gd
@@ -3,14 +3,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("lol")
 	global_position.x = 2000
-	global_position.y = 0
+	global_position.y = 27
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	global_position.x -= delta * 0.3 * 2404
-	if global_position.x < -500:
+	if global_position.x < -1000:
 		queue_free()  # Remove off-screen obstacles
--- Scripts/Levels/level.gd
+++ Scripts/Levels/level.gd
@@ -6,36 +6,71 @@ extends Node2D
 @export var cactus_obstacle_scene: PackedScene  # Assign your obstacle scene here
 @export var spawn_min_interval: float = 1.0  # Minimum spawn interval (seconds)
 @export var spawn_max_interval: float = 3.0  # Maximum spawn interval (seconds)
+@export var score_label: Label  # Assign a Label node for displaying the score
+
+var high_score: int = 0
+var high_score_file: String = "user://high_score.save"
+
+var score: int = 0  # Player's score
 
 # Node to house all the obstacles (and move them according to the speed)
 
 # Game logic
 var game_speed: float = 0.3
 var spawn_timer: Timer
+var is_game_over = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	high_score = _load_high_score()
+	print(high_score)
 	Global.node_parent = self
-	print(player.global_position)
 	# Setup the spawn timer
 	spawn_timer = Timer.new()
 	add_child(spawn_timer)
+	score_label.global_position.x = (get_viewport_rect().size / 2)[0] + 500
+	score_label.global_position.y = 0
 	var obstacle = cactus_obstacle_scene.instantiate()
 	add_child(obstacle)
 	spawn_timer.one_shot = true  # Ensures it stops after each timeout
 	spawn_timer.timeout.connect(self._on_spawn_timer_timeout)  # Connect timeout signal
 	spawn_timer.start(randf_range(spawn_min_interval, spawn_max_interval))  # Start the first spawn cycle
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("down"):
-		pass
-	if Input.is_action_just_pressed("up"):
-		pass
-	
+func _save_high_score(score: int) -> void:
+	var file = FileAccess.open(high_score_file, FileAccess.ModeFlags.WRITE)
+	if file:
+		file.store_string(str(score))
+		file.close()
+	else:
+		print("Error saving high score!")
+		
+func _load_high_score() -> int:
+	if FileAccess.file_exists(high_score_file):
+		var file = FileAccess.open(high_score_file, FileAccess.ModeFlags.READ)
+		if file:
+			var saved_score = file.get_as_text().to_int()
+			file.close()
+			return saved_score
+	return 0  # Default high score if no file exists
 
+func _process(delta: float) -> void:
+	if not is_game_over:
+		score += int(delta * 150)
+	else:
+		spawn_timer.stop()
+		$Floor.material.set_shader_parameter("speed", Vector2(0, 0))
+		if score > high_score:
+			high_score = score
+			_save_high_score(high_score)
+
+	if score_label:
+		score_label.text = str(score)
+	else:
+		print("Score Label is not assigned or does not exist!")
 	
 	$Floor.material.set_shader_parameter("speed", Vector2(game_speed, 0))
 
 func _on_player_just_hit() -> void:
+	is_game_over = true
 	game_speed = 0
 	$CanvasLayer/Restart.show()
 
